0589385b75c5932489f17dd124a9ecc8160eeeec
Danix43 <daninitu39@gmail.com>
2019-10-12 11:20:54 +0300

    Added Delete endpoint. First stable release.

--- src/main/java/com/danix43/herculesapi/controller/IoTController.java
+++ src/main/java/com/danix43/herculesapi/controller/IoTController.java
@@ -11,6 +11,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -33,7 +34,7 @@ public class IoTController {
 	@Autowired
 	private TermometruRepository termometruRepo;
 
-	@GetMapping("/termometru/all")
+	@GetMapping("/termometre/all")
 	public  ResponseEntity<List<Termometru>> getAll() {
 		return ResponseEntity.ok(termometruRepo.findAll());
 	}
@@ -66,6 +67,17 @@ public class IoTController {
 			Termometru saveEntity = modelMapper.map(requestPayload, Termometru.class);
 			termometruRepo.save(saveEntity);
 			return new ResponseEntity<TermometruDAO>(HttpStatus.CREATED);
+		}
+	}
+	
+	@DeleteMapping(path = "/termometru/{id}/delete")
+	public ResponseEntity<Termometru> deleteById(@PathVariable(name = "id") int id) {
+		Optional<Termometru> databaseEntity = termometruRepo.findById(id);
+		if (databaseEntity.isPresent()) {
+			termometruRepo.delete(databaseEntity.get());
+			return new ResponseEntity<Termometru>(HttpStatus.OK);
+		} else {
+			return new ResponseEntity<Termometru>(HttpStatus.NOT_FOUND);
 		}
 	}
 	
